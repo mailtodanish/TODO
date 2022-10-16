7046f62e3452caa3582ec69720a5d8f76cf63815
Mohd Ahshan Danish <mailtodanish@gmail.com>
2022-10-16 20:22:37 +0530

    bug: Bad indentation

--- api/api.py
+++ api/api.py
@@ -3,15 +3,29 @@ from projects.models import Activity
 from .serializers import ActivitySerializer
 
 # Using DRF generic views to simplify code
+
+
 class ActivityListGeneric(generics.ListCreateAPIView):
-        queryset = Activity.objects.all()
-        serializer_class = ActivitySerializer
+    """
+    Activity List
+    """
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
+
 
 class ActivityDetailGeneric(generics.RetrieveDestroyAPIView):
-        def get_queryset(self):
-            queryset = Activity.objects.filter(pk=self.kwargs["pk"])
-            return queryset
-        serializer_class = ActivitySerializer
+    """
+    Activity Detail
+    """
+
+    def get_queryset(self):
+        queryset = Activity.objects.filter(pk=self.kwargs["pk"])
+        return queryset
+    serializer_class = ActivitySerializer
+
 
 class ActivityCreate(generics.CreateAPIView):
-         serializer_class = ActivitySerializer
+    """
+    Create activity
+    """
+    serializer_class = ActivitySerializer
